5c9975426df267ccac1f727bc432ba5901a5caa9
darlinglele <darlinglele@live.com>
2014-07-23 18:29:16 +0800

    added bi-kmeans function

--- kmeans.py
+++ kmeans.py
@@ -4,23 +4,61 @@ from random import *
 import distance
 
 
-def k_means(X, k, dist=distance.euclid):
+def kmeans(X, k, dist=distance.euclid):
     m, n = shape(X)
     centroids = mat([[randrange(min(X[:, i]), max(X[:, i])) for i in xrange(n)]
                      for x in xrange(k)])
-    # Label of X
-    L = mat(zeros((m, 1)))
+
+    dist_label = zeros((m, 2))
     cluster_changed = True
     while cluster_changed:
         cluster_changed = False
-
-        for idx, x in enumerate(X):
-            l = mat(min([(dist(x, c), i) for i, c in enumerate(centroids)])[1])
-            if l != L[idx, :]:
-                cluster_changed = True
-                L[idx, :] = l
-
+        for i, x in enumerate(X):
+            d, l = min([(dist(x, c) ** 2, j) for j, c in enumerate(centroids)])
+            cluster_changed = cluster_changed or l != dist_label[i, 1]
+            dist_label[i] = d, l
         for i in xrange(k):
-            centroids[i, :] = mean(X[nonzero(L[:, 0].A == i)[0]], axis=0)
+            sub_X = X[nonzero(dist_label[:, 1] == i)[0]]
+            if len(sub_X) != 0:
+                centroids[i, :] = mean(sub_X, axis=0)
+    return dist_label, centroids
 
-    return L
+
+def bi_kmeans(X, k, dist=distance.euclid):
+    m, n = shape(X)
+    centroids = [[mean(X[:, i]) for i in xrange(n)]]
+    dist_label = zeros((m, 2))
+    for i, x in enumerate(X):
+        dist_label[i] = dist(x, centroids[0]) ** 2, 0
+
+    best_label = -1
+    best_sse = inf
+    best_sub_centroids = None
+    while len(centroids) < k:
+        for i, c in enumerate(centroids):
+            sub_X = X[nonzero(dist_label[:, 1] == i)[0]]
+
+            sub_dist_label, sub_centroids = kmeans(sub_X, 2)
+
+            sse = sum(dist_label[nonzero(dist_label[:, 1] != i)[0]]) + sum(
+                sub_dist_label)
+
+            # find the best cluster
+            if best_sse > sse:
+                best_sse = sse
+                best_label = i
+                best_dist_label = sub_dist_label
+                best_sub_centroids = sub_centroids
+
+        # update centroid by the best cluster
+        centroids[best_label] = best_sub_centroids[0]
+        centroids.append(best_sub_centroids[1])
+
+        # update the label of X
+        for i, j in zip(nonzero(dist_label[:, 1] == best_label)[0], xrange(len(best_dist_label))):
+            if best_dist_label[j][1] == 1:
+                dist_label[i] = best_dist_label[j][0], len(centroids) - 1
+            else:
+                dist_label[i, 0] = best_dist_label[j][0]
+    
+    return dist_label, centroids
--- test.py
+++ test.py
@@ -4,6 +4,14 @@ import distance
 from kmeans import *
 
 
+def create_cluster(centroid, size, distance):
+    cluster = []
+    for x in xrange(size):
+        cluster.append(
+            centroid + array([randrange(-distance, distance, 1) for x in xrange(len(centroid))]))
+    return cluster
+
+
 def create_clusters(centroids, size, distance):
     X = []
     for centroid in centroids:
@@ -38,23 +46,25 @@ class DistanceTest(unittest.TestCase):
         self.assertEqual(3, dist)
 
 
-def create_cluster(centroid, size, distance):
-    cluster = []
-    for x in xrange(size):
-        cluster.append(
-            centroid + array([randrange(-distance, distance, 1) for x in xrange(len(centroid))]))
-    return cluster
-
-
 class KMeansTest(unittest.TestCase):
 
-    def test_k_means(self):
+    def test_kmeans(self):
         X = create_clusters([(20, 30), (20, 60), (30, 45), (40, 60)], 30, 8)
-        labels = k_means(X, 4)
+        dist_label, centroids = kmeans(X, 4)
         icons = ['b_', 'b.', 'bo', 'b+', 'b*']
 
-        for idx, l in enumerate(labels):
-            plt.plot(X[idx, 0], X[idx, 1], icons[int(l)])
+        for idx, l in enumerate(dist_label):
+            plt.plot(X[idx, 0], X[idx, 1], icons[int(l[1])])
+        # plt.show()
+
+    def test_bi_kmeans(self):
+        X = create_clusters(
+            [(20, 30), (20, 60), (40, 60), (70, 60), (90, 60),(35, 45)], 30, 8)
+        dist_label, centroids = bi_kmeans(X, 6)
+        icons = ['b_', 'b.', 'bo', 'b+', 'b*', 'r.', 'r*']
+        print dist_label
+        for idx, l in enumerate(dist_label):
+            plt.plot(X[idx, 0], X[idx, 1], icons[int(l[1])])
 
         plt.show()
 
